2ee6d3a45302fb59e9b481d40c923edea0decd82
mggons <malagonsxp@hotmail.com>
2021-04-12 16:33:02 -0500

    Update link2.js

--- src/link2.js
+++ src/link2.js
@@ -1,3 +1,11 @@
-  
-const help = (prefijo) => {
-	devolución `
+const link2 = (prefix) => {
+	return `
+┏━━❉ *Acerca de los Grupos* ❉━━━┓
+┣⊱ _*Nombre*_ : Soporte y Aportes BOT
+┣⊱ *Grupo WP* : https://chat.whatsapp.com/DgbbzASKf1M6J6s53GO5hx
+┣⊱ *Grupo TG* : https://t.me/soporteyapps
+┣⊱ *Creador* : JDMTECH
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+}
+
+exports.link2 = link2
