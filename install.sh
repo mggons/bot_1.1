61b950782be18ce767bb6d0d38bed30f0ac9804f
mggons <malagonsxp@hotmail.com>
2021-05-11 18:35:42 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 
 apt-get update
 apt-get upgrade
-apt-get install nodejs -y
-apt-get install libwebp -y
-apt-get install ffmpeg -y
-apt-get install wget -y
-apt-get install tesseract -y
+pkg install nodejs -y
+pkg install libwebp -y
+pkg install ffmpeg -y
+pkg install wget -y
+pkg install tesseract -y
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 
