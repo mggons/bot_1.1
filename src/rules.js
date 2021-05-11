8b8d28b57784ce0cd6145ad00271e580c951d9ed
mggons <malagonsxp@hotmail.com>
2021-05-11 18:18:41 -0500

    Create rules.js

---
+++ src/rules.js
@@ -0,0 +1,56 @@
+const rules = (prefix) => {
+	return `
+
+┏━━❉ *Acerca del BOT* ❉━━━┓
+┣⊱ _*Nombre*_ : Soporte y Aportes BOT
+┣⊱ *Grupo WP* : https://tinyurl.com/syagroup
+┣⊱ *Web Pag*  : https://tinyurl.com/soporteyaportes
+┣⊱ *Grupo TG* : https://t.me/soporteyapps
+┣⊱ *Creador* : JDMTECH
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━
+
+┏━━❉ *Comandos Disponibles* ❉━━━┓
+┣⊱ *Comandos de Administrador y Usuario* 
+┣⊱ ===============================
+┣⊱ ${prefix}admincreator -> Admin
+┣⊱ ${prefix}blocklist -> Lista bloqueados
+┣⊱ ${prefix}setprefix -> Cambia el codigo inicial del grupo
+┣⊱ ${prefix}welcome -> Bienvenida para grupos en general
+┣⊱ ${prefix}welcomusic -> Bienvenida para grupos de musica
+┣⊱ ${prefix}listadmins -> Enlista de admins del grupo 
+┣⊱ ${prefix}linkgroup -> link del grupo
+┣⊱ ${prefix}promote -> Promueve a administrador (solo admins)
+┣⊱ ${prefix}demote -> Degarda de admin a user (solo admins)
+┣⊱ ${prefix}tagall -> Enlista a todos con etiqueta 
+┣⊱ ${prefix}gb -> Mensaje Global medio de difusion (solo admins)
+┣⊱ ${prefix}add -> Añade numeros al grupo (solo admins)
+┣⊱ ${prefix}kick -> Elimina a usuario del grupo (solo admins)
+┣⊱ ${prefix} Proximamente mas comandos
+┣⊱ ===============================
+┣⊱     *Conversion de Imagenes*
+┣⊱ ===============================
+┣⊱ ${prefix}ocr -> Convertir imagen a ocr
+┣⊱ ${prefix}toimg -> Convierte un sticker a imagen
+┣⊱ ${prefix}stiker -> Convierte imagen a sticker
+┣⊱ ===============================
+┣⊱ 	*Comandos de Musica*
+┣⊱ ===============================
+┣⊱ ${prefix}ytbuscar -> Busca en youtube 
+┣⊱ ${prefix}mp3 -> Sacar pista de audio de Youtube
+┣⊱ ${prefix}mp4 -> Sacar video de Youtube
+┣⊱ ================================
+┣⊱ 	     *Texto a Voz*
+┣⊱ ================================
+┣⊱ ${prefix}tts -> Permite pasar de texto a Voz
+┣⊱ ================================
+┣⊱ 	 *Verficador de Links*
+┣⊱ ================================
+┣⊱ ${prefix}mediafire -> Busca el contenido del link de mediafare
+┣⊱ ================================
+┣⊱  Proximamente mas comandos
+┣⊱ ${prefix} 
+┣⊱ ${prefix} 
+┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━`
+}
+
+exports.rules = rules
